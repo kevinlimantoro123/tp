55e2ecea1f8bdcf622afc5ecf92e41a756212f44
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2025-04-06 19:53:28 +0800

    Fix bugs

--- src/main/java/seedu/address/logic/parser/NoteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/NoteCommandParser.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
 
 import seedu.address.commons.core.index.Index;
@@ -27,7 +28,7 @@ public class NoteCommandParser implements Parser<NoteCommand> {
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE), pe);
         }
-
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NOTE);
         if (!argMultimap.getValue(PREFIX_NOTE).isPresent()) {
             throw new ParseException(
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
